d2a38a6747362604d9b6a2d5cedcf82a33a77b1d
Torandori <vaviktova@gmail.com>
2023-08-15 02:16:02 +0200

    Added hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './assets/scss/style.scss'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/My_Project_Unix">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
 
--- src/pages/ServiceDetails.jsx
+++ src/pages/ServiceDetails.jsx
@@ -16,7 +16,7 @@ function ServiceDetails() {
     .then(resp => 
       setDescr(resp)
       )
-    }, [])
+    }, [slug])
   
   const pageTitle = descr.servPageTitle;
 
