695c7124593d05f9b10733a9ba5b7e9345392983
Torandori <vaviktova@gmail.com>
2023-08-29 01:41:10 +0200

    Updates

---
+++ assets/newsDefault-54757a19.js
@@ -0,0 +1,1 @@
+function s(n){const t=new Date(n),a=e=>e<10?"0"+e:e;return`${a(t.getDate())}.${a(t.getMonth()+1)}.${t.getFullYear()}`}const o="/My_Project_Unix/assets/newsDefault-d5aab6d4.jpg";export{s as f,o as n};
