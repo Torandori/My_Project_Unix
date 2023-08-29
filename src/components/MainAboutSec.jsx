aa7a2056438ca7c11ff427efefe4c84587e4c160
Torandori <vaviktova@gmail.com>
2023-08-29 03:13:06 +0200

    Improve optimisation

--- src/components/MainAboutSec.jsx
+++ src/components/MainAboutSec.jsx
@@ -15,13 +15,13 @@ function MainAboutSec() {
         <div className="subtext">{aboutData.aboutSubtext}</div>
         <div className="wrap">
           <p>{aboutData.aboutDescr}</p>
-          <Link to={aboutData.aboutLinkTo} className="small-link" aria-label="Move to services page">{aboutData.aboutLinkText}</Link>
+          <Link to={aboutData.aboutLinkTo} className="small-link" title="Move to services page">{aboutData.aboutLinkText}</Link>
         </div>
         <div id="main-services" className="accordion panchang">
           {/* <ul className="panchang"> */}
             {aboutAccord.map((item, index) => {
                 return (
-                  <div tabIndex="1" key={index} className="accordion-item">
+                  <div key={index} className="accordion-item">
                     <div className="accord-header">
                       <div className="accord-title">{item.aboutAccordName}</div>
                       <div className="arrow-btn">
