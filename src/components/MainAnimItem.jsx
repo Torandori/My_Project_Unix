7318480e8edda13be3eda0a4993fe64e7a13dd92
Torandori <vaviktova@gmail.com>
2023-09-05 17:56:30 +0200

    Fixed some elements for mobile

--- src/components/MainAnimItem.jsx
+++ src/components/MainAnimItem.jsx
@@ -2,22 +2,31 @@
 import { useEffect, useRef } from 'react';
 import VanillaTilt from 'vanilla-tilt';
 import '../assets/scss/_tiltCardProj.scss';
+import { useMediaQuery } from 'react-responsive';
 
 function MainAnimItem({item}) {
+  const isMobile = useMediaQuery({ maxWidth: 880 });
   const any = useRef(null)
 
   useEffect(() => {
-    VanillaTilt.init(any.current, {
-      max: 25,
-      speed: 400,
-      glare: true,
-      "max-glare": 0.3
-    });
-  }, []);
+    console.log('isMobile:', isMobile);
+    if (!isMobile) {
+      VanillaTilt.init(any.current, {
+        max: 25,
+        speed: 400,
+        glare: true,
+        "max-glare": 0.3
+      });
+    }
+  }, [isMobile]);
+
   return (
-    <div className="tiltElProj" ref={any}>
-      <img src={item.projImgUrl} alt={item.projAlt} />
-    </div>
+    // <div className='tiltElProj' ref={any}>
+    //   <img src={item.projImgUrl} alt={item.projAlt} />
+    // </div>
+        <div className='tiltElProj' ref={any}>
+        <img src={item.projImgUrl} alt={item.projAlt} />
+      </div>
   )
 }
 export default MainAnimItem;
