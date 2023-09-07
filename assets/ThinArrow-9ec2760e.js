97cfaa474a2d640ce1c9d7bdb22fa258a00deb65
Torandori <vaviktova@gmail.com>
2023-09-08 01:01:38 +0200

    Updates

--- assets/ThinArrow-9ec2760e.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-346064e6.js";function l(){return e.jsx("svg",{width:"20",height:"20",viewBox:"0 0 20 20",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:e.jsx("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M14.6549 4.16667H3.3334V2.5H17.5001V16.6667H15.8334V5.34518L3.92265 17.2559L2.74414 16.0774L14.6549 4.16667Z",fill:"#121212"})})}export{l as default};
